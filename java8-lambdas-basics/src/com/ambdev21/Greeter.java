5da941c94fb009b63c21418ead12eb40106491eb
Amin Boudeffa <aboudeffa@gmail.com>
2018-01-03 12:51:24 +0100

    Basic greet function setup

---
+++ java8-lambdas-basics/src/com/ambdev21/Greeter.java
@@ -0,0 +1,14 @@
+package com.ambdev21;
+
+public class Greeter {
+
+	public void greet(){
+		System.out.println("Hello world!");
+	}
+	
+	public static void main(String[] args) {
+		Greeter greeter = new Greeter();
+		greeter.greet();
+	}
+
+}
