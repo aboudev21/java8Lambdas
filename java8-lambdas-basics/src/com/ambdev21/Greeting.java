8616169cd68c2d669360a6424e2e432d9ad1be27
Amin Boudeffa <aboudeffa@gmail.com>
2018-01-06 00:13:48 +0100

    Functional Interface

--- java8-lambdas-basics/src/com/ambdev21/Greeting.java
+++ java8-lambdas-basics/src/com/ambdev21/Greeting.java
@@ -1,5 +1,21 @@
 package com.ambdev21;
 
+/**
+ * Marking an interface as a kind of a type interface for a lambda expression
+ * @author BOUDEFFA AMIN
+ *
+ */
+
+@FunctionalInterface
 public interface Greeting {
+	
+	// Only one abstract method
 	public void perform();
+	
+	default void display(){
+		System.out.println("dummmy");
+	}
+	static String get(){
+		return ("foo");
+	}
 }
